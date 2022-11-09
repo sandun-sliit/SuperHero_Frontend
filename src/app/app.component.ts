16f7c07d9d8e17f67c287c7f016da80b6f5a79f0
Sandun <sandunharshana19@gmail.com>
2022-11-09 13:41:40 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SuperHero } from './models/super-hero';
+import { SuperHeroService } from './services/super-hero.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SuperHero.UI';
+  heroes: SuperHero[] = []; //Emty array
+  heroToEdit?: SuperHero;
+
+
+  constructor(private superHeroService: SuperHeroService) {}
+
+  ngOnInit() : void {
+    //this.heroes = this.superHeroService.getSuperHeroes();
+    //console.log(this.heroes);(
+    this.superHeroService.getSuperHeroes().subscribe((result: SuperHero[]) => (this.heroes = result));
+  }
+
+  updateHeroList(heroes: SuperHero[]){
+    this.heroes = heroes;
+  }
+
+  initNewHero(){
+    this.heroToEdit = new SuperHero();
+  }
+
+  editHero(hero: SuperHero){
+    this.heroToEdit = hero;
+  }
+
 }
